ae3925d5f5e3805af466ab5de3cabb89170f1363
gonzarascon <gonzarascon@gmail.com>
2021-06-22 00:08:52 -0300

    fix(generate-aliases): Fixed json composition

--- utils/generate-aliases.ts
+++ utils/generate-aliases.ts
@@ -1,3 +1,4 @@
+import { NumberPropertyValue } from '@notionhq/client/build/src/api-types';
 import { promises as fs } from 'fs';
 import path from 'path';
 import NotionService from '../lib/Notion';
@@ -5,9 +6,12 @@ import NotionService from '../lib/Notion';
 const GenerateAliases = async () => {
   const allPages = await NotionService().getDatabase();
 
-  const parsedObjects = allPages.map((page) =>
-    NotionService().sanitizePage(page)
-  );
+  const parsedObjects = allPages
+    .filter((page) => {
+      const order = page.properties.Order as NumberPropertyValue;
+      return order.number !== 0;
+    })
+    .map((page) => NotionService().sanitizePage(page));
 
   await fs.writeFile(
     path.join(process.cwd(), 'data/aliases.json'),
